ccfe4c847659707fec6be7d851ee128814218e36
LinuZvision <dev@linuzvision.com>
2026-09-22 10:57:24 +0100

    Corrige le build Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+// @ts-check
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   poweredByHeader: false,
   allowedDevOrigins: ["10.32.188.234", "10.96.206.234"],
 };
